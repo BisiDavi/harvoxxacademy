b67c4933934ca07211bbc40a8ae8cd2e9d55c743
BisiDavi <oludavidconnect@gmail.com>
2020-09-15 02:41:45 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use TCG\Voyager\Facades\Voyager;
