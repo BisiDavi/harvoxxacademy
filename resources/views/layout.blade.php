1e597ed487d9afff8f550a7b6523ecaa424cb190
BisiDavi <oludavidconnect@gmail.com>
2020-09-15 01:36:04 +0100

    .

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <!-- include link page -->
+  @include('headlinks')
+  @yield('link')
+</head>
+
+<body>
+  <!-- include the header -->
+  @include('header')
+
+
+
+  @yield('content')
+
+  <!-- include the footer -->
+  @include('footer')
+
+  <a href="#" style="background-color: #7F2AA7;" class="back-to-top">
+    <i class="icofont-simple-up"></i>
+  </a>
+
+  @include('footerlinks')
+
+</body>
+
+</html>
